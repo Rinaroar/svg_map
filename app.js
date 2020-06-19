d5f427af05dea26d40fd26653bda3c889d1e154e
Rinaroar <marina.luxin@gmail.com>
2020-06-19 14:53:34 +0200

    same behavior on list and map + refacto

--- app.js
+++ app.js
@@ -10,17 +10,28 @@ DECLARATION
 FONCTIONS
 */
 
-  paths.forEach(function (path) {
-    path.addEventListener('mouseenter', function (e) {
-
-      let id = this.id.replace('region-', '');
-
-      map.querySelectorAll('.is-active').forEach(function(item) {
+  let activeArea = id => {
+    map.querySelectorAll('.is-active').forEach((item) => {
         item.classList.remove('is-active');
       })
+    document.querySelector('#list-FR-' + id).classList.add('is-active');
+    document.querySelector('#region-' + id).classList.add('is-active');
+  }
 
-      document.querySelector('#list-FR-' + id).classList.add('is-active');
-      document.querySelector('#region-' + id).classList.add('is-active');
 
+  paths.forEach((path) => {
+    path.addEventListener('mouseenter', function () {
+
+      let id = this.id.replace('region-', '');
+      activeArea(id);
+    })
+  })
+
+
+  links.forEach((link) => {
+    link.addEventListener('mouseenter', function () {
+
+      let id = this.id.replace('list-FR-', '');
+      activeArea(id);
     })
   })
